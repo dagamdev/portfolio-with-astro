d6c44f3c4b1b4f5cd7be00f7e7363658def45547
Dagadev <gacub20ru@gmail.com>
2024-01-15 20:49:57 -0600

    add vue folder component & add icons & add Analytics component

---
+++ src/components/vue/Analytics.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+import { io } from 'socket.io-client'
+import EyeIcon from './icons/Eye.vue'
+import HeartIcon from './icons/Heart.vue'
+import HeartFilledIcon from './icons/HeartFilled.vue'
+import UserIcon from './icons/User.vue'
+import { SERVER_PATH } from '../../utils/constants'
+import type { AnalyticsData, SocketData } from '../../types' 
+  
+const count = ref(0)
+const analytics = ref({
+  views: 1,
+  likes: 0,
+  users: 1,
+  liked: false
+})
+
+let browserID = localStorage.getItem('browserID') as string
+
+if (!browserID) {
+  browserID = crypto.randomUUID()
+  localStorage.setItem('browserID', browserID)
+}
+
+const handleAnalytics = (analyticsData: AnalyticsData) => {
+  analytics.value = {
+    views: analyticsData.views,
+    likes: analyticsData.likes,
+    users: analyticsData.browsers.length,
+    liked: analyticsData.browsers.some(b => b.id == browserID && b.liked)
+  }
+}
+
+const socket: SocketData = io(SERVER_PATH)
+
+socket.on('connect', () => {
+  console.log('connect')
+  socket.emit('addView', { browserID })
+
+  socket.on('analytics', handleAnalytics)
+})
+
+onUnmounted(() => {
+  socket.off('analytics', handleAnalytics)
+})
+
+function handleLiked() {
+  if (analytics.value.liked) {
+    socket.emit('removeLike', { browserID })
+  } else {
+    socket.emit('addLike', { browserID })
+  }
+
+  analytics.value.liked = !analytics.value.liked
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="count++">Addition</button>
+
+  <ul class="flex gap-x-4">
+    <li class="flex gap-x-1 items-center">
+      <EyeIcon />
+      <strong>{{ analytics.views }}</strong>
+    </li>
+    <li class="flex gap-x-1 items-center">
+      <span v-if="analytics.liked" class="text-red-500">
+        <HeartFilledIcon @click="handleLiked" class="cursor-pointer" />
+      </span>
+      <HeartIcon v-else @click="handleLiked" class="cursor-pointer" />
+      <strong>{{ analytics.likes }}</strong>
+    </li>
+    <li class="flex gap-x-1 items-center">
+      <UserIcon class="" />
+      <strong>{{ analytics.users }}</strong>
+    </li>
+  </ul>
+</template>
