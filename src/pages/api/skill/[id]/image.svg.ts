40442769814b080cd791a84e6d9c62aed0d13d08
Dagadev <gacub20ru@gmail.com>
2024-01-20 12:58:07 -0600

    Add skill image

---
+++ src/pages/api/skill/[id]/image.svg.ts
@@ -0,0 +1,32 @@
+import type { APIContext } from 'astro'
+import { SKILLS } from '../../../../library/notion'
+
+export async function GET ({ params }: APIContext) {
+  const { id } = params
+  const skill = SKILLS.find(f => f.id === id)
+
+  if (skill === undefined) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not found'
+    })
+  }
+
+  if (skill.icon === null) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not found'
+    })
+  }
+
+  const response = await fetch(skill.icon.url)
+  const svg = await response.text()
+
+  return new Response(svg, {
+    headers: { 'Content-Type': 'image/svg+xml' }
+  })
+}
+
+export function getStaticPaths () {
+  return SKILLS.map(s => ({ params: { id: s.id } }))
+}
